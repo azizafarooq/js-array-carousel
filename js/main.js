6a2887d31884e649e5f8fadd3488f41272c8e3f3
azizafarooq <azizafarooq2000@gmail.com>
2024-01-22 16:36:11 +0100

    reset: html and css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-const images = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp'];
-
+const images=['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp'];
 const carouselContainer = document.querySelector('.carousel-container');
 const carousel = document.querySelector('.carousel');
 const buttonUp = document.querySelector('.button-up');
 const buttonDown = document.querySelector('.button-down');
-
 let currentIndex = 0;
 
 
@@ -34,5 +32,4 @@ buttonDown.addEventListener('click', function () {
   currentIndex = (currentIndex + 1) % images.length;
   updateActiveImage();
 })
-
 
